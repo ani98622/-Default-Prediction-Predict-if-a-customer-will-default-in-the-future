be2f0a4f32344a8c2d6cbb78dcb49b2932cce74a
ani98622 <aniket945478232@gmail.com>
2024-03-06 10:02:01 +0530

    Components

---
+++ app.py
@@ -0,0 +1,16 @@
+from src.Credit_Defaultor_Prediction.logger import logging
+from src.Credit_Defaultor_Prediction.exception import CustomException
+from src.Credit_Defaultor_Prediction.components.data_ingestion import DataIngestionConfig
+import sys
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion
+       
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
